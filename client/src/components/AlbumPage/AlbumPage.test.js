7e1eb8ac9c7b8d759a9ab73762d548ff66e2b090
Naomi Cherotich <naomirono1454@gmail.com>
2024-02-18 17:25:20 +0300

    test: update AlbumPage test for improved mock setup and readability

--- client/src/components/AlbumPage/AlbumPage.test.js
+++ client/src/components/AlbumPage/AlbumPage.test.js
@@ -1,45 +1,41 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import AlbumPage from './AlbumPage';
+import { BrowserRouter, } from 'react-router-dom';
 import axios from 'axios';
+import AlbumPage from './AlbumPage';
 
 jest.mock('axios');
-jest.mock('../Loader/Loader', () => {
-  return {
-    __esModule: true,
-    default: jest.fn(() => null), 
-    createPortal: jest.fn((children, container) => {
-      return children; 
-    }),
-  };
-});
 
+jest.mock('../Loader/Loader', () => ({
+  __esModule: true,
+  default: jest.fn(() => null),
+  createPortal: jest.fn((children) => children),
+}));
 describe('AlbumPage Component', () => {
-  test('renders album information', async () => {
-    const mockAlbumData = {
-      id: 1,
-      userId: 2,
-      title: 'Test Album',
-    };
+  beforeEach(() => {
+    axios.get.mockImplementation((url) => {
+      if (url.includes('/albums/')) {
+        return Promise.resolve({ data: { id: 1, userId: 1, title: 'Test Album' } });
+      } else if (url.includes('/photos?albumId=')) {
+        return Promise.resolve({ data: [{ id: 1, title: 'Photo 1', thumbnailUrl: 'url/to/photo1' }] });
+      }
+      return Promise.reject(new Error('Invalid URL'));
+    });
+  });
 
-
-    axios.get.mockImplementationOnce(() => Promise.resolve({ data: mockAlbumData }));
-
+  test('renders AlbumPage component with album information', async () => {
     render(
       <BrowserRouter>
         <AlbumPage />
       </BrowserRouter>
     );
 
-await waitFor(() => {
-    expect(screen.getByText(/Test Album Album/)).toBeInTheDocument();
-    expect(screen.getByText(/Album ID:/)).toBeInTheDocument();
-    expect(screen.getByText(/User ID:/)).toBeInTheDocument();
-    expect(screen.getByText(/1/)).toBeInTheDocument();
-    expect(screen.getByText(/2/)).toBeInTheDocument();
-  });
-  
+    await waitFor(() => {
+      expect(screen.getByText(/Test Album Album/)).toBeInTheDocument();
+      expect(screen.getByText(/Album ID:/)).toBeInTheDocument();
+      expect(screen.getByText(/User ID:/)).toBeInTheDocument();
     });
 
+  });
+  
 });
